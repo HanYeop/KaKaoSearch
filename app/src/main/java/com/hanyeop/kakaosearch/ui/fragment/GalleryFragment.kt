8065bcda5cffeba46fd2d0de46b7308154b6cebd
Hanyeop <super7615@naver.com>
2021-06-07 14:47:50 +0900

    네비게이션 라이브러리 추가

--- app/src/main/java/com/hanyeop/kakaosearch/ui/fragment/GalleryFragment.kt
+++ app/src/main/java/com/hanyeop/kakaosearch/ui/fragment/GalleryFragment.kt
@@ -1,10 +1,11 @@
-package com.hanyeop.kakaosearch
+package com.hanyeop.kakaosearch.ui.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.hanyeop.kakaosearch.R
 
 class GalleryFragment : Fragment() {
 
