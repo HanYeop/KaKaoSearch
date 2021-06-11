4564efc4b3edd93a429d50e5514adae695f2ca35
Hanyeop <super7615@naver.com>
2021-06-11 22:58:42 +0900

    시스템에 의한 kill이 되었을 때의 처리

--- app/src/main/java/com/hanyeop/kakaosearch/viewmodel/GalleryViewModel.kt
+++ app/src/main/java/com/hanyeop/kakaosearch/viewmodel/GalleryViewModel.kt
@@ -1,18 +1,17 @@
 package com.hanyeop.kakaosearch.viewmodel
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.cachedIn
 import com.hanyeop.kakaosearch.repository.KakaoRepository
 
 class GalleryViewModel @ViewModelInject constructor(
-    private val repository: KakaoRepository
+    private val repository: KakaoRepository,
+    @Assisted state : SavedStateHandle
 ) : ViewModel() {
 
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY,DEFAULT_QUERY)
 
     // 라이브 데이터 변경 시 다른 라이브 데이터 발행
     val images = currentQuery.switchMap { queryString ->
@@ -25,6 +24,7 @@ class GalleryViewModel @ViewModelInject constructor(
     }
 
     companion object {
+        private const val CURRENT_QUERY = "current_query"
         private const val DEFAULT_QUERY = "페이커"
     }
 }
